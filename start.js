152dc44003e934f42c4217fe40afc3382f162823
Victor Bourget <victorbourget@Victors-MBP.centurylink.net>
2024-02-28 22:15:30 -0600

    added start.js script

---
+++ start.js
@@ -0,0 +1,22 @@
+// start.js
+const { exec } = require("child_process");
+
+if (process.env.NODE_ENV === "production") {
+  exec("node index.js", (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+  });
+} else {
+  exec("react-scripts start", (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+  });
+}
